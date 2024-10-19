6f71f02c4ce125fdf44c8e9449524bc0a1173841
SamiraMiss <samirarostami71@gmail.com>
2024-10-20 01:14:11 +0330

    http create server.

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-// Stream
+var http = require("http");
+var fs = require("fs");
 
-const { writeFileSync } = require("fs");
-for (let i = 0; i < 10000; i++) {
-  writeFileSync("./content/big.txt", `hello world ${i}\n`, { flag: "a" });
-}
+http
+  .createServer(function (req, res) {
+    // const text = fs.readFileSync('./content/big.txt', 'utf8')
+    // res.end(text)
+    const fileStream = fs.createReadStream("./content/big.txt", "utf8");
+    fileStream.on("open", () => {
+      fileStream.pipe(res);
+    });
+    fileStream.on("error", (err) => {
+      res.end(err);
+    });
+  })
+  .listen(5000);
